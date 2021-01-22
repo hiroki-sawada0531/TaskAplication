2d1334f7900d03eaa9a1e73d6e9ac8efdca5c979
澤田大樹 <hiroki.bigtree@icloud.com>
2021-01-22 22:29:54 +0900

    作成したコンポーネントの登録2

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.app')
 @section('content')
 <div class="md:mx-4 relative overflow-hidden">
-    <main class="h-full flex flex-col overflow-auto">
-    ぽい</main>
+<main class="h-full flex flex-col overflow-auto">
+    <kanban-board :initial-data="{{ $tasks }}"></kanban-board>
+</main>
 </div>
 @endsection
