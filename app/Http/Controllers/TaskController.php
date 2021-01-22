57ec4c28320ffa269933a242b5aab579695c04c3
澤田大樹 <hiroki.bigtree@icloud.com>
2021-01-22 21:58:30 +0900

    トップページのアクションとビューの作成

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    // ここから
+    public function index()
+    {
+        $tasks = auth()->user()->statuses()->with('tasks')->get();
+
+        return view('tasks.index', compact('tasks'));
+    }
+    // ここまで
 }
