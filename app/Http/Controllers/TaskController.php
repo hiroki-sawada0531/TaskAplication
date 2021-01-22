e1a8ac07104faf015f5204f315df529782ddc584
澤田大樹 <hiroki.bigtree@icloud.com>
2021-01-22 23:13:09 +0900

    データベースに保存

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,13 +6,6 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    /**
-     * タスクカードを追加
-     *
-     * @param Request $request
-     * @return mixed
-     * @throws \Illuminate\Validation\ValidationException
-     */
     public function store(Request $request)
     {
         $this->validate($request, [
@@ -24,5 +17,25 @@ class TaskController extends Controller
         return $request->user()
             ->tasks()
             ->create($request->only('title', 'description', 'status_id'));
+    }
+
+    public function sync(Request $request)
+    {
+        $this->validate(request(), [
+            'columns' => ['required', 'array']
+        ]);
+
+        foreach ($request->columns as $status) {
+            foreach ($status['tasks'] as $i => $task) {
+                $order = $i + 1;
+                if ($task['status_id'] !== $status['id'] || $task['order'] !== $order) {
+                    request()->user()->tasks()
+                        ->find($task['id'])
+                        ->update(['status_id' => $status['id'], 'order' => $order]);
+                }
+            }
+        }
+
+        return $request->user()->statuses()->with('tasks')->get();
     }
 }
