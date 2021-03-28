09bd9e849e88c68abdad3107c17da6f00cf59132
澤田大樹 <hiroki.bigtree@icloud.com>
2021-03-28 17:20:27 +0900

    エラー修正

--- app/Task.php
+++ app/Task.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    protected $fillable = [‘title’, ‘description’, ‘order’, ‘status_id’];
+    protected $fillable = ['title', 'description', 'order', 'status_id'];
 
     public function user()
     {
